7487166a53d00b0e066fcedbe8233d9210b32aca
Urbaeexyz <tazzikra@gmail.com>
2021-03-27 00:43:13 +0700

    fixed kick all

--- HandleMsg.js
+++ HandleMsg.js
@@ -4359,7 +4359,8 @@ console.log(err)
         //Owner Group
         case prefix+'kickall': //mengeluarkan semua member
         if (!isGroupMsg) return aruga.reply(from, 'Maaf, perintah ini hanya dapat dipakai didalam grup!', id)
-        if (!isOwnerB) return aruga.reply(from, 'Maaf, perintah ini hanya dapat dipakai oleh owner Bot karna ada bug pada fitur ini!\n\nsilahkan ketik /ownerbot untuk meminta bantuan fitur /kickall\n\nBest regards\n-Thoriq~', id)
+		let isOwnerGroup = chat.groupMetadata.owner == serial
+        if (!isOwnerGroup && !isOwnerB) return aruga.reply(from, 'Maaf, perintah ini hanya dapat digunakan oleh Owner Grup!', id)
         if (!isBotGroupAdmins) return aruga.reply(from, 'Gagal, silahkan tambahkan bot sebagai admin grup!', id)
             const allMem = await aruga.getGroupMembers(groupId)
             for (let i = 0; i < allMem.length; i++) {
